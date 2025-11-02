314f35502a1e9e4cda244df86327b8070e0cbe56
Manish Goyal <manish@devitechnosolutions.com>
2025-11-02 08:17:01 +0530

    Updated vite.config.ts

--- server/vite.ts
+++ server/vite.ts
@@ -76,11 +76,10 @@ export function serveStatic(app: Express) {
     );
   }
 
-  // Serve static files under /rportal
-  app.use("/rportal", express.static(distPath));
+  app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist (under /rportal path)
-  app.use("/rportal/*", (_req, res) => {
+  // fall through to index.html if the file doesn't exist
+  app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
